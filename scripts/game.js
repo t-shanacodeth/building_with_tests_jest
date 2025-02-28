08639508c3e29c8b1f5dc72d551b07d0eddc374c
Tishana B. <t.bascombe@outlook.com>
2025-02-28 15:23:17 +0000

    first game test

---
+++ scripts/game.js
@@ -0,0 +1,3 @@
+let game = {
+    score: 0,
+};
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,3 +2,19 @@
  * @jest-environment jsdom
  * @jest-environment-options {"url": "https://jestjs.io/"}
  */
+
+const { game } = require("../game");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+});
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    });
+});
